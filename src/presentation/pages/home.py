c8f013f5fd1bbd5c7e08172a43557072ff3bebc5
EngRPin <rafaelluizpinto28@gmail.com>
2025-06-21 23:31:29 -0400

    Implementando erro e carregamento

--- src/presentation/pages/home.py
+++ src/presentation/pages/home.py
@@ -6,22 +6,39 @@ from logic.profile import ProfileService
 from presentation.pages.list_investor_profiles_view import list_investor_profiles_view
 from presentation.pages.profile_details_view import profile_details_view
 
+
 def render_home():
     st.set_page_config(layout="wide")
 
     if "selected_profile_id" in st.session_state:
-        profile_details_view(st.session_state["selected_profile_id"])
+        with st.spinner("Carregando detalhes do perfil..."):
+            profile_details_view(st.session_state["selected_profile_id"])
         return
-    
-    col1, col2 = st.columns([1.5, 1])  # Ajuste conforme quiser
+
+    col1, col2 = st.columns([1.5, 1])
 
     with col1:
-        st.plotly_chart(construir_mapa(), use_container_width=True)
+        with st.spinner("Carregando mapa..."):
+            try:
+                fig = construir_mapa()
+                st.plotly_chart(fig, use_container_width=True)
+                st.markdown("""
+                            <style>
+                                .modebar {
+                                    right: auto !important;
+                                    left: 100px !important;
+                                }
+                            </style>
+                            """, unsafe_allow_html=True)
+            except Exception as e:
+                st.error(f"Erro ao carregar o mapa: {e}")
 
     with col2:
-        profile_repository = ProfileRepositoryMockup()
-        profile_service = ProfileService(repository=profile_repository)
-        all_profiles = profile_service.get_all_profiles()
-        list_investor_profiles_view(profiles=all_profiles)
-
-       
+        with st.spinner("Carregando perfis..."):
+            try:
+                profile_repository = ProfileRepositoryMockup()
+                profile_service = ProfileService(repository=profile_repository)
+                all_profiles = profile_service.get_all_profiles()
+                list_investor_profiles_view(profiles=all_profiles)
+            except Exception as e:
+                st.error(f"Erro ao carregar os perfis: {e}")
